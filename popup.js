bec20c0d446bb985156c2b7dfb2075c6978fbcca
Bruno Henri <brunohenri.g@live.com>
2022-03-01 11:35:19 -0300

    Correções e melhorias

--- popup.js
+++ popup.js
@@ -1,10 +1,6 @@
-let changeColor = document.getElementById("changeColor");
+let refreshPriceButton = document.getElementById("refreshPriceButton");
 
-chrome.storage.sync.get("color", ({ color }) => {
-    changeColor.style.backgroundColor = color;
-});
-
-changeColor.addEventListener("click", async () => {
+refreshPriceButton.addEventListener("click", async () => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
     chrome.scripting.executeScript({
@@ -14,24 +10,31 @@ changeColor.addEventListener("click", async () => {
 });
 
 function filterAllDroppedPrices() {
-    chrome.storage.sync.get("color", ({ color }) => {
-        const allDropPrices = [...document.querySelectorAll("#g-items li.g-item-sortable div.itemPriceDrop")];
-        let allPromotions = [];
+    let prices = [];
 
-        for (let i = 0; i < allDropPrices.length; i++) {
-            const dropPriceElement = allDropPrices[i];
-
-            const spanDropPrice = dropPriceElement.querySelector('[id^=itemPriceDrop_]');
-            const nameOfProduct = dropPriceElement.closest('div.a-column').querySelector('div > h2 > a').innerText;
-            const currentPrice = dropPriceElement.closest('div.a-column').querySelector('span.a-offscreen').innerText.match(/[\d]*,[\d]*/)[0];
-            const previousPrice = dropPriceElement.innerText.match(/[\d]*,[\d]*/)[0];
-
-            if (!spanDropPrice.innerText.startsWith('Queda')) {
-                const discount = spanDropPrice.innerText.trim().replace(/^(\D*)/, '');
-                allPromotions.push({ name: nameOfProduct, currentPrice, previousPrice, discount });
-            }
-        }
-
-        console.log(allPromotions.sort((a, b) => { return b.discount.match(/\d+/) - a.discount.match(/\d+/); }));
+    chrome.storage.sync.get("mainStorage", (data) => {
+        console.log(data);
+        prices = data.mainStorage.prices;
     });
+
+    const allDropPrices = [...document.querySelectorAll("#g-items li.g-item-sortable div.itemPriceDrop")];
+    let allPromotions = [];
+
+    for (let i = 0; i < allDropPrices.length; i++) {
+        const dropPriceElement = allDropPrices[i];
+
+        const spanDropPrice = dropPriceElement.querySelector('[id^=itemPriceDrop_]');
+        const nameOfProduct = dropPriceElement.closest('div.a-column').querySelector('div > h2 > a').innerText;
+        const currentPrice = dropPriceElement.closest('div.a-column').querySelector('span.a-offscreen').innerText.match(/[\d]*,[\d]*/)[0];
+        const previousPrice = dropPriceElement.innerText.match(/[\d]*,[\d]*/)[0];
+
+        if (!spanDropPrice.innerText.startsWith('Queda')) {
+            const discount = spanDropPrice.innerText.trim().replace(/^(\D*)/, '');
+            allPromotions.push({ name: nameOfProduct, currentPrice, previousPrice, discount });
+        }
+    }
+
+    prices.push(...allPromotions.sort((a, b) => { return b.discount.match(/\d+/) - a.discount.match(/\d+/); }));
+
+    chrome.storage.sync.set({ mainStorage: { prices } });
 }
